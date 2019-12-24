cb19868634efc8ab4cd7e0e45166ab08515c8ac9
Filip Todoroski <ftodoroski@ymail.com>
2019-12-24 04:44:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,3 +73,13 @@ end
 def full?(board)
     board.none? { |pos| pos == " " }
 end
+
+def draw?(board)
+    if full?(board) && !won?(board)
+        return true
+    elsif !full?(board) && !won?(board)
+        return false
+    elsif won?(board)
+        return false  
+    end
+end
