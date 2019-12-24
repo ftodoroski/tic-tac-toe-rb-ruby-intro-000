07136b6324ac0e6b1ef4f1387ea6212ec954c433
Filip Todoroski <ftodoroski@ymail.com>
2019-12-24 03:11:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,5 +30,5 @@ def position_taken?(board, position)
 end
 
 def valid_move?(board, position)
-  position.between?(0, 9) && !position_taken?(board, position)
+  position.between?(1, 9) && !position_taken?(board, position)
 end
